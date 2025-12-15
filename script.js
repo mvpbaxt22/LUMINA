667ff60f017db31403559650bf8f1fac083afcf3
mvpbaxt22 <elproiker52@gmail.com>
2025-12-15 00:41:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// FULLSCREEN SIMPLE
-document.querySelectorAll('.photo').forEach(img => {
-  img.addEventListener('click', () => {
-    if (document.fullscreenElement) {
-      document.exitFullscreen();
-    } else {
-      img.requestFullscreen();
-    }
-  });
-});
+
